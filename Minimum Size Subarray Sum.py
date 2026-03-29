3d005c208c36aadc4ab5e24a64da598c6cef3fcf
Kanak Wanjari <wanjarikanak@gmail.com>
2026-03-29 22:59:37 +0530

    1 Problem

--- Minimum Size Subarray Sum.py
+++ Minimum Size Subarray Sum.py
@@ -1,16 +1,63 @@
-nums = [2,3,1,2,4,3]
-target = 7
+def msss(target, nums):
 
-left = 0
-current_sum = 0
-min_length = float('inf')
+    left = 0
+    curr_sum = 0
+    min_length = float('inf')
 
-for right in range(len(nums)):
-    current_sum = current_sum + nums[right]
+    for right in range(len(nums)):
+        curr_sum += nums[right]
 
-    while current_sum >= target:
-        min_length = min(min_length, right-left+1)
-        current_sum = current_sum - nums[left]
-        left = left + 1
+        while curr_sum >= target:
+            min_length = min(min_length, right - left + 1)
+            curr_sum -= nums[left]
+            left += 1
+    
+    if min_length == float('inf'):
+        return 0
+    else:
+        return min_length
+    
+print(msss(7,[2,3,1,2,4,3]))
+    
 
-print(0 if min_length == float('inf') else min_length   )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# nums = [2,3,1,2,4,3]
+# target = 7
+
+# left = 0
+# current_sum = 0
+# min_length = float('inf')
+
+# for right in range(len(nums)):
+#     current_sum = current_sum + nums[right]
+
+#     while current_sum >= target:
+#         min_length = min(min_length, right-left+1)
+#         current_sum = current_sum - nums[left]
+#         left = left + 1
+
+# print(0 if min_length == float('inf') else min_length)
