62eaa84436da450a34073261bde08f28ba692b35
Kanak Wanjari <wanjarikanak@gmail.com>
2026-03-31 20:45:08 +0530

    1 problem

---
+++ Max Consecutive Ones.py
@@ -0,0 +1,14 @@
+def mco(nums):
+    count = 0 
+    max_count = 0
+
+    for i in range(len(nums)):
+        if nums[i] == 1:
+            count += 1
+            max_count = max(max_count, count)
+        else:
+            count = 0
+    
+    return max_count
+
+print(mco([1,1,1,0,1,1,1,1,0,1]))
