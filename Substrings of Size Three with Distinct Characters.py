a1880353b0dd10db29949f30c981f19f22fcb517
Kanak Wanjari <wanjarikanak@gmail.com>
2026-04-04 18:32:46 +0530

    2 Problems

---
+++ Substrings of Size Three with Distinct Characters.py
@@ -0,0 +1,14 @@
+def sostwdc(s):
+    count = 0
+
+    for i in range(len(s) - 2):
+        a = s[i]
+        b = s[i + 1]
+        c = s[i + 2]
+
+        if a != b and b != c and a != c:
+            count += 1
+
+    return count
+
+print(sostwdc("aababcabc"))
--- permutation-in-string.py
+++ permutation-in-string.py
@@ -1,26 +1,84 @@
-s1 = "ab"
-s2 = "eidbaooo"
+def pis(s1,s2):
 
-n = len(s1)
-m = len(s2)
+    n = len(s1)
+    m = len(s2)
 
-if n>m:
-    print("String 1 is longer than String 2")
+    if n > m:
+        return False
+    
+    count_1 = {}
+
+    for i in range(n):
+        count_1[s1[i]] = count_1.get(s1[i], 0) + 1
+
+    for j in range(m - n + 1):
+        count_2 = {}
+
+        for k in range(j, j+n):
+            count_2[s2[k]] = count_2.get(s2[k], 0) + 1
+
+        if count_1 == count_2:
+            return True
+        
+    return False
+
+print(pis("ab","eidbaooo"))
+
+    
+
+    
 
 
-count1 = {}
-for ch in s1:
-    count1[ch] = count1.get(ch,0) + 1
 
-for i in range(m-n+1):
-    window = s2[i:i+n]
 
-    count2 = {}
-    for ch in window:
-        count2[ch] = count2.get(ch,0) + 1
 
-    if count1 == count2:
-        print("True")
-        exit()
 
-print("False")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# s1 = "ab"
+# s2 = "eidbaooo"
+
+# n = len(s1)
+# m = len(s2)
+
+# if n>m:
+#     print("String 1 is longer than String 2")
+
+
+# count1 = {}
+# for ch in s1:
+#     count1[ch] = count1.get(ch,0) + 1
+
+# for i in range(m-n+1):
+#     window = s2[i:i+n]
+
+#     count2 = {}
+#     for ch in window:
+#         count2[ch] = count2.get(ch,0) + 1
+
+#     if count1 == count2:
+#         print("True")
+#         exit()
+
+# print("False")
