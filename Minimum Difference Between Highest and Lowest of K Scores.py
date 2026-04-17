72ada3ddee38360e60fab4ab9a5feff2a8f2cf1d
Kanak Wanjari <wanjarikanak@gmail.com>
2026-04-17 20:06:58 +0530

    1 problem

--- Minimum Difference Between Highest and Lowest of K Scores.py
+++ Minimum Difference Between Highest and Lowest of K Scores.py
@@ -6,7 +6,7 @@ def mdbhaloks(nums, k):
 
     min_diff = float('inf')
 
-    for i in range(0, len(nums)-k + 1):
+    for i in range(len(nums) -  k + 1):
         curr_diff = nums[i + k - 1] - nums[i]
         min_diff = min(min_diff, curr_diff)
 
